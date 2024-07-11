57c64350664c12105f0ad7de26901934b376e964
serdarbicici-visualstudio <serdarbicici.visualstudio@gmail.com>
2024-07-11 17:05:34 +0300

    added Tree class and some methods

--- tree_main.cpp
+++ tree_main.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
+#include "tree_node.cpp"
+
+int main() {
+
+    Tree tree;
+    tree.add_leaf(5);
+    tree.add_leaf(3);
+    tree.add_leaf(7);
+    tree.add_leaf(2);
+    tree.add_leaf(4);
+    tree.add_leaf(6);
+    tree.add_leaf(8);
+    tree.add_leaf(1);
+
+    tree.print_tree();
 
 
+    return 0;
+}
--- tree_node.cpp
+++ tree_node.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
+
 
 class Node
 {
 private:
-    int data;
+    //int data;
 
 public:
+    int data;
     Node *right;
     Node *left;
     
@@ -21,4 +24,102 @@ Node::Node(int number) {
 
 Node::~Node()
 {
+}
+
+class Tree
+{
+private:
+    Node *root;
+
+public:
+    Tree();
+
+    void add_leaf(int number);
+    void delete_leaf(int number);
+    void print_tree();
+
+    ~Tree();
+};
+
+Tree::Tree()
+{
+    root = nullptr;
+}
+
+Tree::~Tree()
+{
+}
+
+void Tree::add_leaf(int number) {
+
+
+    Node *new_node = new Node(number);
+    
+    if (root == nullptr) {
+        root = new_node;
+        return;
+    }
+
+    Node *current = root;
+
+    while (true)
+    {
+        if (number > current->data)
+        {
+            if (current->right == nullptr)
+            {
+                current->right = new_node;
+                return;
+            }
+            current = current->right;
+        }
+        else
+        {
+            if (current->left == nullptr)
+            {
+                current->left = new_node;
+                return;
+            }
+            current = current->left;
+        }
+    }
+    
+    
+
+}
+
+void Tree::print_tree() {
+    // in order traversal recursive
+    // left, root, right
+    Node *current = root;
+    
+    while (current != nullptr)
+    {
+        if (current->left == nullptr)
+        {
+            std::cout << current->data << std::endl;
+            current = current->right;
+        }
+        else
+        {
+            Node *pre = current->left;
+            while (pre->right != nullptr && pre->right != current)
+            {
+                pre = pre->right;
+            }
+
+            if (pre->right == nullptr)
+            {
+                pre->right = current;
+                current = current->left;
+            }
+            else
+            {
+                pre->right = nullptr;
+                std::cout << current->data << std::endl;
+                current = current->right;
+            }
+        }
+    }
+    
 }
